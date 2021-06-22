a6802ed622060a39cfe00333525e6bcc79713c24
Igor <igoramarals96@gmail.com>
2021-06-22 20:40:37 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,110 @@
+particlesJS({
+    "particles": {
+      "number": {
+        "value": 284,
+        "density": {
+          "enable": false,
+          "value_area": 4261.397264814273
+        }
+      },
+      "color": {
+        "value": "#8c8c8c"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 1,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 7
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5932624625202434,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 0,
+          "opacity_min": 0.07308694910712106,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#020000",
+        "opacity": 0.5852454022159158,
+        "width": 1.4430708547789706
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.5,
+        "direction": "top",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 1763.753266952075,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "remove"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": false
+  });
